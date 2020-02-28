0806868daa48cbb6d8ea9099213d960ce64b2ba9
itcast-hsian <407775611@qq.com>
2020-02-28 10:57:57 +0800

    tabbar配置和首页轮播图

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+import request from "./utils/request.js";
+
 //app.js
 App({
-  onLaunch: function () {
-    
-  }
+    onLaunch: function() {
+        // 指定一个基准路径
+        request.defaults.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,23 @@
+import request from "../../utils/request.js";
+
 Page({
-  
+
+    data: {
+        // 轮播图的数据
+        banners: []
+    },
+
+    onLoad(){
+        // 请求轮播图接口
+        request({
+            url: "/home/swiperdata"
+        }).then(res => {
+            // message是轮播图的数组
+            const {message} = res.data;
+            // 赋值给banners
+            this.setData({
+                banners: message
+            })
+        })
+    }
 })
