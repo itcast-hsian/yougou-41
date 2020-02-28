c1b21ea7f678984074aab01a504bfb9fdf3db56b
itcast-hsian <407775611@qq.com>
2020-02-28 12:21:58 +0800

    导航菜单和楼层布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,9 @@ Page({
 
     data: {
         // 轮播图的数据
-        banners: []
+        banners: [],
+        // 菜单的数组
+        menus: []
     },
 
     onLoad(){
@@ -17,6 +19,29 @@ Page({
             // 赋值给banners
             this.setData({
                 banners: message
+            })
+        })
+
+        // 请求中间导航菜单的接口
+        request({
+            url: "/home/catitems"
+        }).then(res => {
+            // message是菜单的数组
+            const { message } = res.data;
+
+            // 循环添加跳转链接
+            const newData = message.map( (v, i) => {
+                // 代表是分类
+                if(i === 0){
+                    v.url = "/pages/category/index"
+                }
+
+                return v;
+            })
+
+            // 赋值给menus
+            this.setData({
+                menus: newData
             })
         })
     }
