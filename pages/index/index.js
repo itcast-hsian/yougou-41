2ca571786a024e0b54c9ed1100ae114153ba0311
itcast-hsian <407775611@qq.com>
2020-02-28 15:15:40 +0800

    回到顶部

--- pages/index/index.js
+++ pages/index/index.js
@@ -69,4 +69,25 @@ Page({
         })
     },
 
+
+    // 监听页面的滚动事件
+    onPageScroll(e){
+        const { scrollTop } = e;
+        // 当前的状态
+        let isShow = this.data.isShowTop;
+        
+        // 判断如果滚动高度大于100，显示回到顶部的按钮
+        if (scrollTop > 100){
+            isShow = true
+        }else{
+            isShow = false
+        }
+
+        // 避免频繁的操作setData，所以如果下面两个值是相同就没必要再赋值了
+        if (isShow == this.data.isShowTop) return;
+
+        this.setData({
+            isShowTop: isShow
+        })
+    }
 })
