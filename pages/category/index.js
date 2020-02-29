5c2adfe5019c9d1f84e42e2e2da713f787245cf5
itcast-hsian <407775611@qq.com>
2020-02-29 10:38:36 +0800

    分类左侧菜单点击高亮

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,7 +5,8 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        // 记录当前点击了哪个菜单
+        current: 0,
     },
 
     /**
@@ -13,5 +14,16 @@ Page({
      */
     onLoad: function (options) {
 
-    }  
+    },
+
+    // 点击切换左边菜单栏时候触发
+    handleClick(e){
+        // index参数
+        const {index} = e.currentTarget.dataset;
+        
+        // 保存当前点击的索引到data
+        this.setData({
+            current: index
+        })
+    }
 })
