46b5327769226fca357d59ce2b26573abc8c3a7b
itcast-hsian <407775611@qq.com>
2020-02-29 11:31:09 +0800

    请求和渲染分类页的数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/category/index.js
 Page({
 
@@ -7,13 +9,24 @@ Page({
     data: {
         // 记录当前点击了哪个菜单
         current: 0,
+        // 数据列表
+        list: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // 请求分类页的数据
+        request({
+            url: "/categories"
+        }).then(res => {
+            // 把数据列表保存到data
+            const {message} = res.data;
+            this.setData({
+                list: message
+            })
+        })
     },
 
     // 点击切换左边菜单栏时候触发
