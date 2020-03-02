863f4d005063105e538d1e3971c0e362ae65240d
itcast-hsian <407775611@qq.com>
2020-03-02 14:34:42 +0800

    判断是否正常加载中，加上开关功能

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -13,7 +13,9 @@ Page({
         // 是否有更多
         hasMore: true,
         // 页面
-        pagenum: 1
+        pagenum: 1,
+        // 是否正在加载中
+        loading: true
     },
 
     /**
@@ -34,6 +36,12 @@ Page({
 
     // 请求商品列表数据
     getGoods(){
+
+        // 如果没有更多了，就不会再请求
+        if(this.data.hasMore == false){
+            return;
+        }
+
         setTimeout(v => {
             // 请求商品列表
             request({
@@ -56,8 +64,17 @@ Page({
                 // 把message商品列表保存到list
                 this.setData({
                     // 合并原来的列表和新请求回来的商品列表
-                    goods: [...this.data.goods, ...goods]
-                })
+                    goods: [...this.data.goods, ...goods],
+                    // 当前这次请求完毕
+                    loading: false
+                });
+
+                // 判断是否是最后一页
+                if (this.data.goods.length >= message.total) {
+                    this.setData({
+                        hasMore: false
+                    })
+                }
             })
 
         }, 2000)
@@ -65,12 +82,19 @@ Page({
 
     // 页面上拉触底时候触发
     onReachBottom(){
-        // 页数加1
-        this.setData({
-            pagenum: this.data.pagenum + 1
-        });
 
-        // 请求商品列表
-        this.getGoods();
+        // 需要等到上一次的请求回来了再执行下一页的数据
+        if(this.data.loading === false){
+
+            this.setData({
+                // 每次发起请求前重新设置loadig为正在加载
+                loading: true,
+                // 页数加1
+                pagenum: this.data.pagenum + 1
+            });
+
+            // 请求商品列表
+            this.getGoods();
+        }
     }
 })
