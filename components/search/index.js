894641725c44ad5b2711f0067e45855e7fc364c6
itcast-hsian <407775611@qq.com>
2020-03-02 15:29:01 +0800

    自定义组件的扩展（样式，props）

--- components/search/index.js
+++ components/search/index.js
@@ -4,8 +4,21 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        // 组件可以传递的值
+        keyword: {
+            type: String,
+            value: "搜索" // 这个是默认值，类似于vue里面的default
+        },
+        // 通过这个属性控制文字展示的位置
+        align: {
+            type: String,
+            value: "center" // left，center，right
+        }
     },
+
+    // 外部扩展的样式,声明了可以接收background这个样式
+    // align输入框的文字居中还是左对齐
+    externalClasses: ['background',"align"],
 
     /**
      * 组件的初始数据
