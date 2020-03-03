c78abe8fa740181469cfe6222ab8f63bf055bb35
itcast-hsian <407775611@qq.com>
2020-03-03 10:51:33 +0800

    搜索页的布局，监听输入框的输入来进行搜索建议的查询

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,11 +1,15 @@
-// pages/search/index.js
+import request from "../../utils/request.js"
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        // 输入框的值
+        inputValue: "",
+        // 搜索建议
+        recommend: []
     },
 
     /**
@@ -15,52 +19,29 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+    // 监听输入框的输入事件
+    handleInput(e){
+        // value是输入框的值
+        const {value} = e.detail;
+        this.setData({
+            inputValue: value
+        });
 
-    },
+        // 如果value有值才发起请求
+        if(!value) return;
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+        // 请求搜索建议
+        request({
+            url: "/goods/qsearch",
+            data: {
+                query: value
+            }
+        }).then(res => {
+            const {message} = res.data;
+            // 保存到搜索建议的数组
+            this.setData({
+                recommend: message
+            })
+        })
     }
 })
