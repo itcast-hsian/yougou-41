cdf012cd1e0a86ead8df91d7cc488d387d3dc99a
itcast-hsian <407775611@qq.com>
2020-03-06 11:12:21 +0800

    把收货保存到本地

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -13,7 +13,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // 获取本地的收货地址
+        this.setData({
+            // 如果本地没有address就等于一个空对象
+            address: wx.getStorageSync("address") || {}
+        })
     },
 
     // 获取收货地址
@@ -23,7 +27,7 @@ Page({
             success: (res) => {
                 // 把收货地址保存到data
                 this.setData({
-                   address: {
+                    address: {
                        // 收货人
                        name: res.userName,
                        // 手机号码
@@ -31,7 +35,10 @@ Page({
                        // 详细地址
                        detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
                    }
-                })
+                });
+
+                // 保存到本地
+                wx.setStorageSync('address', this.data.address);
             }
         })
     }
