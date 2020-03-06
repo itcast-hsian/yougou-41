d13ceaba8afa38f7fc376229a7d9e42aeab15675
itcast-hsian <407775611@qq.com>
2020-03-06 14:05:25 +0800

    把商品添加到本地的购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -66,5 +66,44 @@ Page({
         wx.switchTab({
             url: '/pages/cart/index',
         })
+    },
+
+    // 把商品加入到本地的购物车列表
+    handleAddCart(){
+        // 每次加入商品之前应该先判断本地有没有数据，如果没有就等于一个空数组
+        const goods = wx.getStorageSync("goods") || [];
+
+        // 判断当前的商品是否已经在goods的数组中
+        // 存在就数量加一，不存在就unshift
+        // some循环数组，return的结果“只要有一个是true就会返回true”，反之就false
+        const exit = goods.some(v => {
+            // 通过id判断当前商品是否在本地的数组中
+            const isExit = v.goods_id === this.data.detail.goods_id;
+            // 存在就数量加一
+            if (isExit){
+                v.number += 1;
+                // 提示，文档地址https://developers.weixin.qq.com/miniprogram/dev/api/ui/interaction/wx.showToast.html
+                wx.showToast({
+                    title: '数量+1',
+                    icon: 'success'
+                })
+            }
+
+            return isExit;
+        })
+
+        if (!exit){
+            // 把当前的商品添加到本地的数组中
+            goods.unshift({
+                goods_id: this.data.detail.goods_id,
+                goods_name: this.data.detail.goods_name,
+                goods_price: this.data.detail.goods_price,
+                goods_small_logo: this.data.detail.goods_small_logo,
+                number: 1
+            })
+        }
+
+        // 保存到本地
+        wx.setStorageSync("goods", goods);
     }
 })
