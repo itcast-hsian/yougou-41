a72a1cff0b9ff1f04a944f2daf6d2c030b08616c
itcast-hsian <407775611@qq.com>
2020-03-10 10:43:46 +0800

    个人中心页的布局和样式

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -25,5 +25,19 @@ Page({
                 selected: 3
             })
         }
+    },
+
+    // 收货地址
+    handleAddress(){
+        // 根据接口的名字在文档里搜索
+        wx.chooseAddress();
+    },
+
+    // 联系客服
+    handleContact(){
+        // 根据接口的名字在文档里搜索
+        wx.makePhoneCall({
+            phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+        })
     }
 })
